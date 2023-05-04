f4c8c10a4371ab4c9be91829edad7b6bb8a2f6bd
Transcendence <baydindenys@ukr.net>
2023-05-04 19:30:43 +0300

    contacts upd

---
+++ contacts.js
@@ -0,0 +1,27 @@
+// Import of modules
+// const path = require('node:path'); Метод модуля Path
+const contactsPath = require('./db/contacts.json');
+const fs = require('fs');
+const path = require('path');
+import { readFile, writeFile } from 'node:fs';
+
+
+// TODO: задокументировать каждую функцию
+// function listContacts() {
+    // ...твой код
+//   }
+  
+//   function getContactById(contactId) {
+    // ...твой код
+//   }
+  
+//   function removeContact(contactId) {
+    // ...твой код
+//   }
+  
+//   function addContact(name, email, phone) {
+    // ...твой код
+//   }
+
+
+// module.exports = func;
