693f80b8ba8fd0774420ffe8e5e6ad92ce717f0e
Transcendence <baydindenys@ukr.net>
2023-05-06 16:04:08 +0300

    code update
    code update

--- contacts.js
+++ contacts.js
@@ -1,27 +1,29 @@
-// Import of modules
-// const path = require('node:path'); Метод модуля Path
-const contactsPath = require('./db/contacts.json');
-const fs = require('fs');
-const path = require('path');
-import { readFile, writeFile } from 'node:fs';
+// Import
+const fs = require("fs/promises");
+const path = require("node:path");
+const contactsPath = require("db/contacts.json");
 
+const getContacts = async () => {
+  const contact = await fs.readFile(contactsPath, require("buffer"));
+  return JSON.parse(contacts);
+};
 
 // TODO: задокументировать каждую функцию
-// function listContacts() {
+function listContacts() {
     // ...твой код
-//   }
+  }
   
-//   function getContactById(contactId) {
+  function getContactById(contactId) {
     // ...твой код
-//   }
+  }
   
-//   function removeContact(contactId) {
+  function removeContact(contactId) {
     // ...твой код
-//   }
+  }
   
-//   function addContact(name, email, phone) {
+  function addContact(name, email, phone) {
     // ...твой код
-//   }
+  }
 
 
-// module.exports = func;
+module.exports = { getContacts };
--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log("Hello World 2023");
+// Import
+// const fs = require("fs/promises");
+// const contacts = fs.require("./db");
+
+const dataContacts = async ({ id, name, email, phone }) => {
+    switch (action) {
+        case "ready":
+            const allContact = await contacts.getContacts();
+            return console.log(allContact);
+    }
+};
